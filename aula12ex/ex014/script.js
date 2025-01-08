435add1bb6ef7c9deb8bae4eb8ec876893f0b45b
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-01-08 17:21:35 -0300

    arrumei os erros

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -3,12 +3,11 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'fotomanha.jpg'
         document.body.style.background = '#e2cd9f'
-    } else if (hora >= 12 && <18) {
+    } else if (hora >= 12 && hora <= 18 ) {
         img.src = 'fototarde.jpg'
         document.body.style.background = '#b9846f'
     } else {
