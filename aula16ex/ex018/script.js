81da7332f49af21dd4d74e0e11effd336b1d244f
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-01-12 15:07:07 -0300

    atualizando ex018

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -13,14 +13,33 @@ function isNumero(n) {
 
 function inLista(n, l) {
     if (l.indexOf(Number(n)) != -1) {
-        
+        return true
+    } else {
+        return false
     }
 }
 
-function adicionar () {
-    if (isNumero(num.value) ** !inLista(num.value)) {
-       
+function adicionar() {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+       valores.push(Number(num.value))
+       let item = document.createElement('option')
+       item.text = `Valor ${num.value} adicionado`
+       lista.appendChild(item)
+       res.innerHTML = ''
     } else {
          window.alert('Valor inválido ou já encontrado na lista!')
+    }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if(valores.length == 0) {
+        alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+    
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
     }
 }
